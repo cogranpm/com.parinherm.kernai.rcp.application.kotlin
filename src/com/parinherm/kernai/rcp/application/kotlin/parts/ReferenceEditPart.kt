e274fefb600c255ce5f9aef629cbc6691e6a246e
paulm <cogranpm@gmail.com>
2019-10-04 20:41:11 -0500

    more databding stuff

--- src/com/parinherm/kernai/rcp/application/kotlin/parts/ReferenceEditPart.kt
+++ src/com/parinherm/kernai/rcp/application/kotlin/parts/ReferenceEditPart.kt
@@ -34,6 +34,12 @@ import org.eclipse.core.databinding.AggregateValidationStatus
 import org.eclipse.core.databinding.Binding
 import java.time.LocalTime
 import org.eclipse.swt.widgets.Button
+import org.eclipse.swt.widgets.Spinner
+import org.eclipse.jface.databinding.fieldassist.ControlDecorationSupport
+import org.eclipse.core.databinding.conversion.NumberToStringConverter
+import org.eclipse.core.databinding.validation.ValidationStatus
+
+import kotlin.text.*
 
 class ReferenceEditPart {
 	
@@ -49,6 +55,9 @@ class ReferenceEditPart {
 	private lateinit var dteCreated: DateTime
 	private lateinit var dteCreatedTime: DateTime
 	private lateinit var btnBool: Button
+	private lateinit var spinInt: Spinner
+	private lateinit var txtLong: Text
+	
 	private lateinit var lblError: Label
 	
 	val model = ReferenceItemViewModel()
@@ -79,13 +88,23 @@ class ReferenceEditPart {
 		lblBool.text = "Boolean"
 		btnBool = Button(parent, SWT.CHECK)
 		
+		var lblInt = Label(parent, SWT.NONE)
+		lblInt.text = "Integer"
+		spinInt = Spinner(parent, SWT.NONE)
+		spinInt.setMinimum(Int.MIN_VALUE)
+		spinInt.setMaximum(Int.MAX_VALUE)
+		
+		var lblLong = Label(parent, SWT.NONE)
+		lblLong.text = "Long"
+		txtLong = Text(parent, SWT.SINGLE)
+		txtLong.setLayoutData(GridData(GridData.FILL_HORIZONTAL))
+		
 		
 		lblError = Label(parent, SWT.NONE)
 		val errLayout = GridData(GridData.FILL_HORIZONTAL);
 		errLayout.horizontalSpan = 2
 		lblError.setLayoutData(errLayout)
 
-		
 		val target_body = WidgetProperties.text<Text>(SWT.Modify).observe(txtBody)
 		val model_body = PojoProperties.value<ReferenceItem, String>("body").observeDetail<ReferenceItem>(model.selectedItem)
 		
@@ -100,12 +119,41 @@ class ReferenceEditPart {
 		
 		
 		val propBool = WidgetProperties.buttonSelection().observe(btnBool)
-		var modelBool = PojoProperties.value<ReferenceItem, Boolean>("testBool").observeDetail<ReferenceItem>(model.selectedItem)
+		val modelBool = PojoProperties.value<ReferenceItem, Boolean>("testBool").observeDetail<ReferenceItem>(model.selectedItem)
 		
-		ctx.bindValue(target_body, model_body)
+		val propInt = WidgetProperties.spinnerSelection().observe(spinInt)
+		val modelInt = PojoProperties.value<ReferenceItem, Int>("testInt") .observeDetail<ReferenceItem>(model.selectedItem)
+		
+		val propLong = WidgetProperties.text<Text>(SWT.Modify).observe(txtLong)
+		val modelLong = PojoProperties.value<ReferenceItem, Long>("testLong").observeDetail<ReferenceItem>(model.selectedItem)
+		
+		val bindBody = ctx.bindValue(target_body, model_body)
 		ctx.bindValue(dateTimeObservableValue, model_created)
 		ctx.bindValue(timeOb, model_time)
 		ctx.bindValue(propBool, modelBool)
+		ctx.bindValue(propInt, modelInt)
+		
+		//long integer binding with validation and conversion
+		//the regex allows leading sign symbol and comma's as the
+		//StringToNumberConverter seems to want to put in commas
+		val longConverter = StringToNumberConverter.toLong(true)
+		val longConverterBack = NumberToStringConverter.fromLong(true)
+		val longUpdate = UpdateValueStrategy.create(longConverter)
+		longUpdate.setAfterGetValidator(
+			{x ->
+				val entry = x as String
+				val regex = "^[+-]?(\\d+(,\\d{3})*)$".toRegex()
+				if(regex.matches(entry)){
+					ValidationStatus.ok()
+				}else {
+					ValidationStatus.error("Invalid number entered")
+				}
+			})
+
+		val longUpdateBack = UpdateValueStrategy.create(longConverterBack)
+		val bindLong = ctx.bindValue(propLong, modelLong, longUpdate, longUpdateBack)
+		
+		ControlDecorationSupport.create(bindLong, SWT.TOP or SWT.LEFT)
 		
 				// set a dirty state if one of the bindings is changed
 		ctx.getBindings().forEach{ item -> 
